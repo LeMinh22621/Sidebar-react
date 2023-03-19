fe0f992f4ad1aa2578db3ca83d124965ea8bbc96
lhm2k1 <minh.lehong@digicommercegroup.com>
2023-03-19 11:29:40 +0700

    custom list video

--- src/components/video/Video.js
+++ src/components/video/Video.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactPlayer from 'react-player';
 import './Video.css';
 
-function VideoBackground()  {
+function VideoBackground({url})  {
+  const [play,setPlay]=useState(true)
+  console.log(play)
+const PlayVid=()=>{
+  setPlay(!play)
+  console.log("play dc ne: ",play)
+
+}
+  console.log("VideoBackground said: ",play,url)
       return (
-        <div className="video-background">
-          <div className='overlay'></div>
-          <ReactPlayer className='react-player' url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
-       
+        <div className="video-background" onClick={()=>setPlay(!play)}>
+         
+          <ReactPlayer
+           playing={play}
+           
+                className='video-item'
+                url={url}
+                sources={[
+                    { src: '<https://www.youtube.com/watch?v=IbJLWZaTcQs>', type: 'video/mp4' },
+                    { src: '<https://www.youtube.com/watch?v=IbJLWZaTcQs>', type: 'video/webm' },
+                ]}
+                tracks={[
+                    { src: '<https://example.com/subtitles.vtt>', kind: 'subtitles', srclang: 'en' },
+                    { src: '<https://example.com/captions.vtt>', kind: 'captions', srclang: 'en' },
+                ]}
+            />
         </div>
       );
 }
---
+++ src/components/video/VideoData.jsx
@@ -0,0 +1,5 @@
+ export const VideoData= [{"video_id":1, "user_id":1, "link":"https://www.youtube.com/shorts/jqoj6z51T8M", "title":"Console ninja"},
+ {"video_id":2, "user_id":1, "link":"https://www.youtube.com/shorts/qlY6f8dyXTw", "title":"Suzume "},
+ {"video_id":3, "user_id":2, "link":"https://www.youtube.com/shorts/ZtyMdRzvi0w", "title":"God Tier Html "},
+ {"video_id":4, "user_id":2, "link":"https://www.youtube.com/shorts/4KBlOGg2lRo", "title":"Your name "}
+]
